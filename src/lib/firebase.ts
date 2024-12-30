2869c72da82977acbe14a0915606a2044e58a3f4
David Dewanto <david.dewanto@gmail.com>
2024-12-30 10:46:24 +0700

    modify firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -15,4 +15,8 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
+
 export { auth, googleProvider };
